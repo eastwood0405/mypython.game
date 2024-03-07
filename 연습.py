95ce4aed9f6983673a4ef7cfd48e4e11ce7c8c97
eastwood0405 <dongrim.eastwood@gmail.com>
2024-03-07 22:23:42 +0900

    20240307

--- 연습.py
+++ 연습.py
@@ -1,53 +1,63 @@
-for i in range(1,11):
-    print("*"*i)
-for i in range(1,11):
-    print(" "*(10-i)+"*"*(1+2*(i-1)))
-for i in range(10,0,-1):
-    print(" "*(10-i)+"*"*(i*2-1))
-for i in range(1,11):
-    print("*"*(10-i)+" "*(1+2*(i-1))+"*"*(10-i))
-for i in range(10,0,-1):
-    print("*"*(10-i)+" "*(i*2-1)+"*"*(10-i))
-for i in range(10,0,-1):
-    print("*"*i)
-for i in range(1,11):
-    print(" "*(10-i)+"*"*i)
+# for i in range(1,11):
+#     print("*"*i)
+# for i in range(1,11):
+#     print(" "*(10-i)+"*"*(1+2*(i-1)))
+# for i in range(10,0,-1):
+#     print(" "*(10-i)+"*"*(i*2-1))
+# for i in range(1,11):
+#     print("*"*(10-i)+" "*(1+2*(i-1))+"*"*(10-i))
+# for i in range(10,0,-1):
+#     print("*"*(10-i)+" "*(i*2-1)+"*"*(10-i))
+# for i in range(10,0,-1):
+#     print("*"*i)
+# for i in range(1,11):
+#     print(" "*(10-i)+"*"*i)
 
-x=1
-while x <11:
-    print(" "*(10-x)+"*"*(x*2-1))
-    x+=1
+# x=1
+# while x <11:
+#     print(" "*(10-x)+"*"*(x*2-1))
+#     x+=1
 
 
-for x in range(4):
-    if x ==2:
-        break
-    print(x)
-    print(1)
+# for x in range(4):
+#     if x ==2:
+#         break
+#     print(x)
+#     print(1)
       
 
+# for x in range(1,10):
+#     for y in range(x):
+#         print("*",end="")
+#     print("\n",end="")
+
+#파이썬 print("hello world")
+
+#자료형
+# 정수 int 1,2,3,5
+# 실수 float0.1,0.2
+# 문자열 str "1"."python"
+# type()으로 알 수 있다
 
 
 
+# 파이썬의 연산자171252
 
+# 더하기:+
+# 곱하기:*
+# 빼기:-
+# 나누기:/
+# 몫://
+# 나머지:%
+# 거듭제곱**
+# ==같다 != 같지 않다  and,or not
+# <=,>=,<,>
+#==:비교 연산자 =:대입 연산자
 
+#변수:
+# 변수이름 = 13 숫자나 공백 x
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# 내장 자료형 list, set , tuple , dict
 
 
 
---
+++ 연습2.py
@@ -0,0 +1,34 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class BlackBox:
+    def __init__(self,name,price):
+        self.name = name
+        self.price = price
+
+    def set_travel_mode(self,min):
+        print(f"{self.name}을 {str(min)}분동안여행모드on")
+
+b1 = BlackBox("까망이",2000)
+b1.set_travel_mode(100 )
+BlackBox.set_travel_mode(b1,100)
